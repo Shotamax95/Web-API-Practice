e7c9cafca76faf57e430e22d01e58a2cfa3dc850
Shotamax95 <shota.110.free.style@gmail.com>
2021-06-09 14:26:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web API Practice
+This json practice is using async, await fetch.
+
+reference:
+https://jsonplaceholder.typicode.com
---
+++ app.js
@@ -0,0 +1,26 @@
+const lists = document.getElementById("lists");
+const btn = document.getElementById("addBtn");
+
+//retrieve data
+async function getUsers(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    return users;
+}
+
+// DOM
+function addList(user) {
+    const list = document.createElement("li");
+    list.innerText = user.name;
+    lists.appendChild(list);
+}
+
+async function listUsers() {
+    const users = await getUsers();
+    //DOM
+    users.forEach(addList);
+}
+
+// events
+btn.addEventListener("click", listUsers);
+window.addEventListener("load", listUsers);
